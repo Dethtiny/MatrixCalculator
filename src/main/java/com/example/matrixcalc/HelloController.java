7d8e4a333589375d4077b67a2cedc77f88dd28fe
Dethtiny <nikitaushkalov@yandex.ru>
2023-11-15 19:11:32 +0300

    test

--- src/main/java/com/example/matrixcalc/HelloController.java
+++ src/main/java/com/example/matrixcalc/HelloController.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+//test
 
 public class HelloController {
     private static final Logger logger = LogManager.getLogger(HelloController.class.getName());
